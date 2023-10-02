ac08d3436c048319624f6002101541d9f80e0f27
ponmathi <ponmathittl@gmail.com>
2023-10-02 23:27:49 +0530

    verify user email

--- src/MainNavigation.js
+++ src/MainNavigation.js
@@ -1,17 +1,34 @@
-import { NavLink } from "react-router-dom";
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import AuthContext from './store/AuthContext';
 import classes from './MainNavigation.module.css';
+import AuthContext from './store/AuthContext';
+import classes from './MainNavigation.module.css'
 
-const MainNavigation = () =>{
-    return(
-        <header className = {classes.header}>
-            <ul>
-                <li>
-                    <NavLink to='/'>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/Login'>Login</NavLink>
-                </li>
-            </ul>
+const MainNavigation = () => {
+    const navigate = useNavigate();
+    const authCtx = useContext(AuthContext);
+    const logoutHandler = () => {
+        authCtx.logout();
+        navigate('/');
+    };
+
+    return (
+        <header className={classes.header}>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/products">Products</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                </ul>
+            </nav>
+            <button onClick={logoutHandler}>Logout</button>
         </header>
     );
 };
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,89 +1,147 @@
-import React, { useContext, useRef, useState, useEffect } from 'react';
-import AuthContext from '../../store/AuthContext';
-import './Profile.module.css';
+import React, { useRef, useContext, useState, useEffect } from 'react';
+import AuthContext from '../store/AuthContext';
+import './Profile.css'
 
 const Profile = () => {
     const authCtx = useContext(AuthContext);
     const nameInputRef = useRef();
     const imageInputRef = useRef();
-    const [profileData, setProfileData] = useState({ name: '', photoUrl: '' });
-
-    // Function to fetch user profile data
-    const fetchUserProfileData = async () => {
-        const token = authCtx.token;
-        try {
-            const response = await fetch('https://your-firebase-database-url.com/userProfile.json?auth=' + token);
-            if (!response.ok) {
-                throw new Error('Failed to fetch user profile data.');
-            }
-            const data = await response.json();
-            if (data) {
-                setProfileData(data);
-            }
-        } catch (error) {
-            console.error('Error fetching user profile data:', error);
-        }
-    };
-
-    useEffect(() => {
-        // Fetch user profile data when the component mounts
-        fetchUserProfileData();
-    }, []);
-
+    const [userData, setUserData] = useState({
+        displayName: '',
+        photoUrl: ''
+    });
     const submitHandler = async (event) => {
         event.preventDefault();
         const enteredName = nameInputRef.current.value;
         const photoUrl = URL.createObjectURL(imageInputRef.current.files[0]);
         if (!photoUrl) {
-            alert('Please upload an image.');
-            return;
+        console.log("Upload an image");
+        return;
         }
         const token = authCtx.token;
         try {
-            const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=YOUR_API_KEY', {
+            const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCgl1Kkkaw7_gJny8ISnqxhFean3l_05B8', {
                 method: 'POST',
                 body: JSON.stringify({
                     idToken: token,
                     displayName: enteredName,
                     photoUrl: photoUrl,
-                    deleteAttribute: ['DISPLAY_NAME'],
-                    returnSecureToken: true,
+                    deleteAttribute: ["DISPLAY_NAME"],
+                    returnSecureToken: true
                 }),
                 headers: {
-                    'Content-Type': 'application/json',
-                },
+                    'Content-Type': 'application/json'
+                }
             });
+    
             if (response.ok) {
-                alert('Profile updated successfully');
+                alert("Profile updated successfully");
             } else {
-                const errorResponse = await response.json();
-                console.error(errorResponse); // Log the error response for debugging
-                alert('Profile update failed');
+                const responseData = await response.json();
+                console.log(response.status); // Should print 400
+                console.log(responseData.error.message); // Specific error message from Firebase
+                alert("Profile update failed");
             }
-        } catch (error) {
-            console.error('An error occurred:', error);
+        } 
+        catch (error) {
+            console.error("An error occurred:", error);
+        }
+
+    };
+    const verifyEmailHandler = async () => {
+        try {
+            const response = await fetch(
+                'https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCgl1Kkkaw7_gJny8ISnqxhFean3l_05B8',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        requestType: 'VERIFY_EMAIL',
+                        idToken: authCtx.token
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Firebase-Locale': 'en' // Set the language to English
+                    }
+                }
+            );
+  
+            if (!response.ok) {
+                console.error('Request failed with status:', response.status);
+                const responseData = await response.json();
+                console.error('Response data:', responseData);
+                throw new Error('Request failed');
+            }
+  
+            const data = await response.json();
+            console.log('Email verification response:', data);
+            alert('you verification link is sented to email')
+            return data;
+        } 
+        catch (error) {
+            console.error('Error:', error);
+            throw error;
         }
     };
 
+    useEffect(() => {
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCgl1Kkkaw7_gJny8ISnqxhFean3l_05B8', {
+            method: 'POST',
+            body: JSON.stringify({
+                idToken: authCtx.token
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((response) => {
+            if (response.ok) {
+                console.log(response);
+                return response.json();
+            }
+            throw new Error("Failed to fetch user data");
+        })
+        .then((data) => {
+            const user = data.users[0];
+            if (user) {
+                setUserData({
+                    displayName: user.displayName || '',
+                    photoUrl: user.photoUrl || ''
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    }, [authCtx.token]);
+
     return (
-        <form onSubmit={submitHandler}>
-            <div className="mb-3">
-                <label htmlFor="name" className="form-label">Name</label>
-                <input
-                    type="text"
-                    className="form-control"
-                    id="name"
-                    placeholder="Name"
-                    ref={nameInputRef}
-                    defaultValue={profileData.name}
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="photo" className="form-label">Update your photo</label>
-                <input type="file" className="form-control" id="photo" ref={imageInputRef} />
-            </div>
-            <button type="submit" className="btn btn-primary">Update details</button>
-        </form>
+        <div>
+            <form onSubmit={submitHandler} className="profile-form">
+                <div controlId="name">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        ref={nameInputRef}
+                        defaultValue={userData.displayName || ''}
+                    />
+                </div>
+  
+                <div>
+                    <label htmlFor="photo">Update your photo</label>
+                    <input type="file" ref={imageInputRef} />
+                    {userData.photoUrl ? (
+                        <img src={userData.photoUrl} alt="Profile" />
+                    ) : (
+                        <p>No profile picture available</p>
+                    )}
+                </div>
+  
+                <button type="submit">Update details</button>
+            </form>
+  
+            <button onClick={verifyEmailHandler}>Verify email</button>
+        </div>
     );
 };
 
