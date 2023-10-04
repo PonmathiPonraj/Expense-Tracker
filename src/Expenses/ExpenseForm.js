6de2dda01560a6dc50ed107e4adeeb8d47004f00
ponmathi <ponmathittl@gmail.com>
2023-10-04 18:00:01 +0530

    edit and delete expenses

--- src/Expenses/ExpenseForm.js
+++ src/Expenses/ExpenseForm.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import styles from './ExpenseForm.module.css';
 
 const ExpenseForm = () => {
     const [moneySpent, setMoneySpent] = useState('');
     const [description, setDescription] = useState('');
     const [category, setCategory] = useState('');
     const [expenses, setExpenses] = useState([]);
+    const [editingIndex, setEditingIndex] = useState(null); // To track which expense is being edited
+    const [editedMoneySpent, setEditedMoneySpent] = useState('');
+    const [editedDescription, setEditedDescription] = useState('');
+    const [editedCategory, setEditedCategory] = useState('');
 
     const handleMoneySpentChange = (event) => {
         setMoneySpent(event.target.value);
@@ -20,18 +25,12 @@ const ExpenseForm = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // Handle form submission (you can send this data to the backend in the next step).
         const newExpense = {
             moneySpent,
             description,
             category,
         };
-        // Update the expenses array with the new expense
-        setExpenses([...expenses, newExpense]);
-        // Clear the form fields
-        setMoneySpent('');
-        setDescription('');
-        setCategory('');
+        // Send a POST request to add the new expense to the database
         fetch('https://expensetracker-411c3-default-rtdb.firebaseio.com/expenses.json', {
             method: 'POST',
             body: JSON.stringify(newExpense),
@@ -43,28 +42,88 @@ const ExpenseForm = () => {
             if (!response.ok) {
                 throw new Error('Failed to add the expense');
             }
-            // Clear the form fields
+            return response.json();
+        })
+        .then(() => {
+            setExpenses([...expenses, newExpense]);
             setMoneySpent('');
             setDescription('');
             setCategory('');
-            return response.json();
         })
         .catch((error) => {
             console.error(error);
         });
     };
-
-    useEffect(() => {
-        // Perform a GET request to fetch expenses data when the component mounts
-        fetch('https://expensetracker-411c3-default-rtdb.firebaseio.com/expenses.json')
+  
+    const handleEditClick = (index) => {
+        setEditingIndex(index);
+        setEditedMoneySpent(expenses[index].moneySpent);
+        setEditedDescription(expenses[index].description);
+        setEditedCategory(expenses[index].category);
+    };
+  
+    const handleSaveClick = (index) => {
+        const editedExpense = {
+            moneySpent: editedMoneySpent,
+            description: editedDescription,
+            category: editedCategory,
+        };
+  
+        // Make a PUT request to update the edited expense in the database
+        fetch(`https://expensetracker-411c3-default-rtdb.firebaseio.com/expenses/${index}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(editedExpense),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
         .then((response) => {
+            if (!response.ok) {
+                throw new Error('Failed to update the expense');
+            }
+            return response.json();
+        })
+        .then(() => {
+            const updatedExpenses = [...expenses];
+            updatedExpenses[index] = editedExpense;
+            setExpenses(updatedExpenses);
+            setEditingIndex(null);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+  
+    const handleDeleteClick = (index) => {
+        // Make a DELETE request to remove the expense from the database
+        fetch(`https://expensetracker-411c3-default-rtdb.firebaseio.com/expenses${index}.json`, {
+            method: 'DELETE',
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Failed to delete the expense');
+            }
+            return response.json();
+        })
+        .then(() => {
+            const updatedExpenses = expenses.filter((_, i) => i !== index);
+            setExpenses(updatedExpenses);
+            console.log('Expense successfully deleted');
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+    };
+  
+    useEffect(() => {
+        // Fetch expenses data when the component mounts
+        fetch('https://expensetracker-411c3-default-rtdb.firebaseio.com/expenses.json').then((response) => {
             if (!response.ok) {
                 throw new Error('Failed to fetch expenses');
             }
             return response.json();
         })
         .then((data) => {
-            // Process the data as needed and set it in state
             if (data) {
                 const expensesArray = Object.values(data);
                 setExpenses(expensesArray);
@@ -73,15 +132,19 @@ const ExpenseForm = () => {
         .catch((error) => {
             console.error(error);
         });
-    }, []); 
+    }, []);
 
     return (
-        <div>
+        <div className={styles.expenseForm}>
             <h2>Expense Form</h2>
             <form onSubmit={handleSubmit}>
                 <label>
                     Money Spent:
-                    <input type="number" value={moneySpent} onChange={handleMoneySpentChange} />
+                    <input 
+                        type="number"
+                        value={moneySpent}
+                        onChange={handleMoneySpentChange}
+                        className={styles.input}  />
                 </label>
                 <br />
                 <label>
@@ -99,16 +162,46 @@ const ExpenseForm = () => {
                     </select>
                 </label>
                 <br />
-                <button type="submit">Submit</button>
+                <button type="submit" className={styles.button}>
+                    Submit
+                </button>
             </form>
             <h2>Expenses List</h2>
             <ul>
                 {expenses.map((expense, index) => (
                     <li key={index}>
-                        Money Spent: {expense.moneySpent}, Description: {expense.description}, Category: {expense.category}
+                        {editingIndex === index ? (
+                            <>
+                                {/* Edit mode input fields */}
+                                <input
+                                    type="number"
+                                    value={editedMoneySpent}
+                                    onChange={(e) => setEditedMoneySpent(e.target.value)}
+                                />
+                                <input
+                                    type="text"
+                                    value={editedDescription}
+                                    onChange={(e) => setEditedDescription(e.target.value)}
+                                />
+                                <select
+                                    value={editedCategory}
+                                    onChange={(e) => setEditedCategory(e.target.value)}
+                                >
+                                {/* Options for categories */}
+                                </select>
+                                <button onClick={() => handleSaveClick(index)}>Save</button>
+                            </>
+                        ) : (
+                            <>
+                                {/* Display expense details */}
+                                Money Spent: {expense.moneySpent}, Description: {expense.description}, Category: {expense.category}
+                                <button onClick={() => handleEditClick(index)}>Edit</button>
+                                <button onClick={() => handleDeleteClick(index)}>Delete</button>
+                            </>
+                        )}
                     </li>
-                ))}
-            </ul>
+                ))}	   
+            </ul>	    
         </div>
     );
 };
